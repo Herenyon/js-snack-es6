6723a1498a4b4d33b5f73b16e3ae73588e3157ac
Herenyon <rikitur17@gmail.com>
2023-03-09 15:03:50 +0100

    fine esercizio

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -4,23 +4,49 @@ const studentsId = [213, 110, 250, 145, 196, 102, 120];
 const studentsName = ['Marco della Rovere', 'Paola Cortellessa', 'Andrea Mantegna', 'Gaia Borromini', 'Luigi Grimaldello', 'Piero della Francesca', 'Francesca da Polenta'];
 const studentsGrades = [78, 96, 48, 74, 68, 50, 84]
 
-const studentsCard = [];
+const studentsCardFlat = [];
 
 
-for (var i = 0; i < studentsName.length; i++) {
+for (let i = 0; i < studentsName.length; i++) {
     let cardCreate = {
         id: '',
         nome: '',
         grades: '',
     };
-    studentsCard.push(cardCreate);
+    studentsCardFlat.push(cardCreate);
 }
-console.log(studentsCard);
+console.log(studentsCardFlat);
 
-studentsCard.forEach((element, index) => {
+studentsCardFlat.forEach((element, index) => {
     element['id'] = studentsId[index];
     element['nome'] = studentsName[index];
     element['grades'] = studentsGrades[index];
-    console.log(element);
+    
 }); 
+
+
+
+const studentsCard = studentsCardFlat.map(({nome}) => nome.toUpperCase())
+console.log(studentsCard);
+
+const studentsLeaderboard = studentsCardFlat.filter(function(students) {
+
+    if (students.grades > 70){
+        return true;
+    }
+
+})
+
+console.log(studentsLeaderboard);
+
+const studentsTop = studentsCardFlat.filter(function(students){
+
+    if (students.grades > 70 && students.id > 120){
+
+        return true;
+    }
+
+});
+
+console.log(studentsTop);
 
