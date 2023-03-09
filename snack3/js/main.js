2a36604e213bdebf7b0c349c9bcc4a55ed5d4cc8
Herenyon <rikitur17@gmail.com>
2023-03-09 20:27:27 +0100

    lista squadre

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,6 +1,38 @@
 'use strict'
 
 
+const arrayBici = [
+    {
+        nome: 'Specialized Demo-8',
+        kg: 17.2,
+    },
 
+    {
+        nome: 'Santacruz V10',
+        kg: 15.88,
+    },
+
+    {
+        nome: 'Mondraker Summum',
+        kg: 18,
+    },
+
+    {
+        nome: 'YT Industries Tues 29',
+        kg: 15.2,
+    },
+
+    {
+
+        nome: 'Canyon Sender CFR 29',
+        kg: 16.80,
+    },
+]
+
+const biciLeggera = [...arrayBici].sort((a, b) => a.kg - b.kg);
+
+const { nome, kg } = biciLeggera[0];
+
+console.log(`Il nome della Bicicletta è ${nome} e pesa ${kg} chili.`);
 
 
--- snack4/js/main.js
+++ snack4/js/main.js
@@ -1,5 +1,43 @@
 'use strict'
 
+const squadre = [
 
+    {
+        nome: 'Milan',
+        punti: 0,
+        falliSubiti: 0,
+    },
+
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Sassuolo',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Cosenza',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Termoli',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Lamezia Terme',
+        punti: 0,
+        falliSubiti: 0,
+    },
+]
 
 
